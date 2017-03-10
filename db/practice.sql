00f43f1b672f8c0fa25f60c36c6d245dc727d3a2
nturner5 <nturner5@users.noreply.github.com>
2017-03-09 19:34:22 -0700

    added first product on home page, css for home view

--- db/practice.sql
+++ db/practice.sql
@@ -15,3 +15,20 @@ INSERT INTO products (name, size, color, collection, price, popularity, descript
 VALUES ('watchx', 200, 'silver', 'special', 123, true, 'elegant');
 
 -- drop table products;
+
+CREATE TABLE products(
+   id serial PRIMARY KEY,
+   name text,
+   price int,
+   popularity boolean,
+   imageurl text
+);
+
+INSERT INTO products (name, price, popularity, imageurl)  
+VALUES ('Marina Rose Gold Mesh', 149.00, true, '//cdn.shopify.com/s/files/1/1653/9561/products/MARINA_-_MESH_ROSE_GOLD_copy_8a3e118d-cd93-4bb1-9e39-b0920f305ee5_large.png?v=1484417397');
+INSERT INTO products (name, price, popularity, imageurl)  
+VALUES ('watchx', 200, true, 'elegant');
+INSERT INTO products (name, price, popularity, imageurl)  
+VALUES ('watchx', 200, true, 'elegant');
+INSERT INTO products (name, price, popularity, imageurl)  
+VALUES ('watchx', 200, true, 'elegant');
