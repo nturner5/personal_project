eaf6f0c3fc0e9fea2ddf322c0392ad642c1f6b65
nturner5 <nturner5@users.noreply.github.com>
2017-03-15 18:57:43 -0600

    button hovers work on home view, started working on jquery scroll. Works using window but not with specific div

--- public/directive/animations.js
+++ public/directive/animations.js
@@ -1,28 +1,57 @@
 angular.module('app')
-.directive('animationsDirective', function(){
-    return {
-        restrict: 'EA',
-        scope: {
-            animate: '='
-        },
-        link: function(scope, element, attributes){
-            // $(document).ready(function(){
-            //     // all jquery goes here
-            //     $('.collections-name').hover(function(){
-            //         console.log('hover activate')
-            //         $('.feliz-bg').css('background-image', "url('//cdn.shopify.com/s/files/1/1653/9561/t/3/assets/home_collection_2_image.jpg?7955300992394463818')" )
-            //     },
-            //     function(){
-            //         console.log('hover deactivate')
-            //         $('.feliz-bg').css('background-image', "url('//cdn.shopify.com/s/files/1/1653/9561/t/3/assets/home_collection_1_image.jpg?7955300992394463818')" )
-            //     }
-                
-            //     )
-                
-            // })
-        }
-    
-        
-    }
-})
+    .directive('animationsDirective', function () {
+        return {
+            restrict: 'EA',
+            scope: {
+                animate: '='
+            },
+            link: function (scope, element, attributes) {
+                $(document).ready(function () {
+                    //     // all jquery goes here
+                    //     $('.collections-name').hover(function(){
+                    //         console.log('hover activate')
+                    //         $('.feliz-bg').css('background-image', "url('//cdn.shopify.com/s/files/1/1653/9561/t/3/assets/home_collection_2_image.jpg?7955300992394463818')" )
+                    //     },
+                    //     function(){
+                    //         console.log('hover deactivate')
+                    //         $('.feliz-bg').css('background-image', "url('//cdn.shopify.com/s/files/1/1653/9561/t/3/assets/home_collection_1_image.jpg?7955300992394463818')" )
+                    //     }
 
+                    //     )
+
+                    // $(window).scroll(function(){
+                    //     console.log('scroll works')
+                    //     $('html, body').animate({
+                    //         scrollTop: $(".homebg_1").offset().top + $(".homebg_1")[0].scrollHeight
+                    //     }, 1000);
+                    //     return false;
+                    // })
+                    var iScrollPos = 0;
+
+
+
+                    $(window).scroll(function () {
+
+                        var iCurScrollPos = $(this).scrollTop();
+                        if (iCurScrollPos > iScrollPos) {
+                            //Scrolling Down
+                            console.log('down')
+                        } else {
+                            //Scrolling Up
+                            console.log('up')
+                            
+                        }
+                        iScrollPos = iCurScrollPos;
+                    });
+
+
+
+
+
+
+                })
+            }
+
+
+        }
+    })
